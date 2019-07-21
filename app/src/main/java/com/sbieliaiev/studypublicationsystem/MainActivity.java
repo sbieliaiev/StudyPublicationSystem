bad3b4cb3234537502a51582abc8c17c9fb308e3
sbieliaiev <bieliaievstanislav@gmail.com>
2019-07-21 13:30:15 +0300

    First commit

---
+++ app/src/main/java/com/sbieliaiev/studypublicationsystem/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sbieliaiev.studypublicationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
